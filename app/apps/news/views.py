968c602ee5a517e0f7039c9c25dcf6adf0a60221
muladzevitali <muladzevitali@gmail.com>
2021-10-20 16:25:22 +0400

    added list view for news and template for ui

--- app/apps/news/views.py
+++ app/apps/news/views.py
@@ -7,3 +7,12 @@ class NewsListView(ListView):
     model = News
     template_name = 'news.html'
     paginate_by = 10
+
+    def get_queryset(self):
+        query_set = self.model.objects.all()
+        is_trending: str = self.request.GET.get('is_trending', False)
+        if self.request.GET.get('is_trending', False) and is_trending.isdigit():
+            is_trending = bool(int(is_trending))
+            query_set = query_set.filter(is_trending=is_trending)
+
+        return query_set
